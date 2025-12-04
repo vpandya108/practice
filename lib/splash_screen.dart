7c086d1659d64e6f7df6e61799eaf0241f038956
vpandya108 <vpandya108@rku.ac.in>
2025-12-04 18:07:57 +0530

    Refactor SplashScreen to improve layout and add a welcome message

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:prac/home_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -9,20 +12,39 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
-  
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const HomeScreen()),
+      );
+    });
+  }
+
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body:Container(
+    return Scaffold(
+      body: Container(
         color: Colors.blueAccent,
+
         child: Center(
-          child: Text('welcome to car 360',
-          style: TextStyle(
-            fontSize: 30,
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-          ),),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Icon(Icons.directions_car, size: 100, color: Colors.white),
+              SizedBox(height: 20),
+              Text(
+                'CAR 360',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
         ),
-      )
+      ),
     );
   }
 }
