961595beab2a534ce355c780f0f902108fa1c06a
vpandya108 <vpandya108@rku.ac.in>
2025-12-04 17:59:45 +0530

    Add HomeScreen widget with initial StatefulWidget implementation

---
+++ lib/home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
